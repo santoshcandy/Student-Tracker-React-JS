7da0d7965d4501b1fdc3e50cb153800c0745270b
santoshcandy <msanthosh11062003@gmail.com>
2025-01-30 18:24:10 +0530

    student login and dashboard

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {  HashRouter as Router, Route, Routes } from 'react-router-dom';
 import TeacherRegister from './components/TeacherRegister';
 import TeacherLogin from './components/TeacherLogin';
 import ClassList from './components/ClassList';
 import StudentList from './components/StudentList';
 import StudentDashboard from './components/StudentDashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Homepage from './components/Homepage';
+import StudentLogin from './student/StudentLogin';
 const App = () => {
     return (
         <Router>
          
             <Routes>
                 <Route path="/teacher/register" element={<TeacherRegister/>} />
-                <Route path="" element={<TeacherLogin/>} />
+                <Route path="/teacher/login" element={<TeacherLogin/>} />
+                <Route path="/student/login" element={< StudentLogin/>} />
+
+                <Route path="" element={<Homepage/>} />
+
                 <Route path="/teacher/classes" element={<ClassList/>} />
                 <Route path="/teacher/classes/:classId/students" element={<StudentList/>} />
                 <Route path="/teacher/students/:studentId/dashboard" element={<StudentDashboard/>} />
